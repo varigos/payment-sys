25f9142427ac7484d4b053de95e7c0f948891111
Ahmed.H <124135152+varigos@users.noreply.github.com>
2024-04-25 12:12:14 +0300

    config

---
+++ paymoney.php
@@ -0,0 +1,25 @@
+<?php
+
+
+return [
+
+    'demo' => env('APP_DEMO', true),
+    
+    'version' => env('APP_VERSION'),
+
+    'item_code' => '22341650',
+
+    'server_url' => 'https://support.techvill.org',
+
+    'name' => env('APP_NAME', 'Laravel'),
+
+    'mail' => env('APP_MAIL', true),
+
+    'sms' => env('APP_SMS', true),
+
+    'prefix' => env('ADMIN_PREFIX', 'admin'),
+
+    'file_permission' => 0775,
+
+    'api_latest' => 2,
+];
